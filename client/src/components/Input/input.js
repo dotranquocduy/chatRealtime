560d7cc69607e9a7c6a7165e052ef36394d2940c
dotranquocduy <dotranquocduy2000@.com>
2025-06-29 03:04:19 +0900

    update frontend 3

---
+++ client/src/components/Input/input.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+const Input = {
+
+}
+
+export default Input;
--- client/src/components/chat/chat.js
+++ client/src/components/chat/chat.js
@@ -1,6 +1,65 @@
+import queryString from "query-string";
+import { useEffect, useState } from "react";
+import React from "react";
+import { io } from "socket.io-client";
+import Messages from "../messages/messages";
+import Input from "../Input/input";
+import Textcontainer from "../TextContainer/textcontainer";
+import Infobar from "../InfoBar/infobar";
 
 const ENDPOINT = "https://chatrealtime-qd45.onrender.com";
-const chat = ({location}) => {
+let socket;
+const Chat = ({location}) => {
+    const[users, setUsers] = useState('');
+    const [name, setName]= useState("");
+    const [room, setRoom] = useState("");
+    const [notifi, setNotifi] = useState("");
+    const [message, setMessage] = useState([]);
+    useEffect(()=>{
+         const [name, room] = queryString.parse(location.search);
+         socket = io(ENDPOINT);
+         setRoom(room);
+         setName(name);
+
+         socket.emit("join", {name,room}, (error) =>{
+            if(error)
+                alert(error);
+         }); // gui thong tin cho toan bo room 
+    },[ENDPOINT, location.search]);
+    useEffect(()=>{
+       socket.on("message", message =>{
+
+       }) // lang nghe su kien khi thong tin message tu server gui xuong va hien thi man hinh
+
+       socket.on("roomData", ({users}) =>{
+        setUsers(users)
+       }); // lang nghe su kien khi hien thi them thong tin user vao trong room 
+        
+    },[]);
+
+
+    // send message gui tin nhan len server
+    const sendMessage =(event) =>{
+        event.preventDefaut();
+        socket.emit("sendMessage", (message) =>{
+            setMessage((messages) =>[...messages], message);
+        })
+
+    };
+
+
+    return(
+        <div className="outerContainer">
+            <div className="container">
+                <Infobar room = {room}/>
+                <Messages notifi ={notifi} name={name}/>
+                <Input message = {message} setMessage ={setMessage} sendMessage={sendMessage} />
+            </div>
+
+            <Textcontainer />
+            
+        </div>
+    );
     
 }
-export default chat;
+export default Chat;
